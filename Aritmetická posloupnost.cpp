0e8aff7fa1c920bd11546eb6b412ef9e21ac0cf6
adame <adame@DESKTOP-VOPV20U>
2022-01-30 19:07:50 +0100

    Přidejte soubory projektu.

---
+++ Aritmetická posloupnost.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int a, d, n = 20;
+	//a = clen, d = diference
+	printf("Vlozte clen: ");
+	scanf_s("%d",&a);
+	printf("Vlozte diferenci: ");
+	scanf_s("%d", &d);
+	int N = 1;
+	for (int i = 1; i <= 20; i++) {
+		printf("%d ", (a + (N - 1) * d));
+		N++;
+	}
+}
